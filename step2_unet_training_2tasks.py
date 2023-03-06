e1f528ae62b20175414ed564241c7a59168872ef
sizhuoli <33125880+sizhuoli@users.noreply.github.com>
2023-03-06 12:54:44 +0100

    Update step2_unet_training_2tasks.py

--- step2_unet_training_2tasks.py
+++ step2_unet_training_2tasks.py
@@ -6,9 +6,8 @@ Created on Sun Feb 21 00:22:48 2021
 @author: sizhuo
 """
 
-# seg with counting (by regression)
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]="0" # first gpu
+os.environ["CUDA_VISIBLE_DEVICES"]="0" 
 
 from core2.training_segcount import trainer
 
@@ -21,7 +20,7 @@ trainer_segcount.train_config()
 if 'complex' in config.model_name:
     print('complex model')
     trainer_segcount.LOAD_model()
-# check seg loss!!!!!!!!!!!1
+
 trainer_segcount.train()
 trainer_segcount.train_retrain()
 trainer_segcount.train_retrain_eff()
